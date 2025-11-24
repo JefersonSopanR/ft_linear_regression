3a9a6dc78b05c95d94032ec5f1d95d22af0f9a60
JefersonSopanR <jeferson23sopan@gmail.com>
2025-11-24 12:26:04 +0100

    init linearRegression

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ft_linear_regression
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"encoding/csv"
+	"math"
+	"strconv"
+)
+
+type linearRegression struct {
+	learningRate float64
+	theta0 float64
+	theta1 float64
+
+	rawMileages	[]float64
+	rawPrices []float64
+
+	meanMileage float64
+	stdDevMileage float64
+
+	mileages []float64
+	prices []float64
+}
+
+
+func createLinearRegerssion(filename string) *linearRegression {
+	lr := &linearRegression{
+		learningRate: 0.01,
+		theta0: 0.0,
+		theta1: 0.0,
+	}
+
+	lr.readData(filename)
+	lr.standardize()
+
+	return lr
+}
+
+func (lr *linearRegression) readData(filename string) {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	reader:= csv.NewReader(file)
+	records, _ := reader.ReadAll()
+
+	for _, record := range records[1:] {
+		mileage, _ := strconv.ParseFloat(record[0], 64)
+		price, _ := strconv.ParseFloat(record[1], 64)
+
+		lr.rawMileages = append(lr.rawMileages, mileage)
+		lr.rawPrices = append(lr.rawPrices, price)
+	}
+
+}
+
+func (lr *linearRegression) standardize() {
+	sum := 0.0
+
+	// get the mean for the mileages
+	for _, m := range lr.rawMileages {
+		sum += m
+	}
+	lr.meanMileage = sum / float64(len(lr.rawMileages))
+
+	// get the standar Deviation for the mileages -> by how the point are far from the mean on avarege
+	variance := 0.0
+	for _, m := range lr.rawMileages {
+		diff := m - lr.meanMileage
+		variance = diff * diff
+	}
+	lr.stdDevMileage = math.Sqrt(variance / float64(len(lr.rawMileages)))
+
+	for _, m := range lr.rawMileages {
+		scaled := (m - lr.meanMileage) / lr.stdDevMileage
+		lr.mileages = append(lr.mileages, scaled)
+	}
+	lr.prices = lr.rawPrices
+}
+
+
+func main() {
+	lr := createLinearRegerssion("data.csv")
+	for _, rawPrice := range lr.rawPrices {
+		fmt.Println(rawPrice)
+	}
+}
